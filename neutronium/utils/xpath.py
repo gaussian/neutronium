721b547b11bfb4475652214a4b7e43868cf9e8a9
Kut Akdogan <5658236+kut@users.noreply.github.com>
2018-12-06 13:14:52 -0500

    Better catch empty HTML for XML parser

--- neutronium/utils/xpath.py
+++ neutronium/utils/xpath.py
@@ -1,9 +1,11 @@
 import lxml.html
+import lxml.etree.ParserError
 
 from neutron.utils.logging import log_sys_exception
 
 
 def get_xml_doc_from_html(html):
+    # Empty or missing
     if not html:
         return None
 
@@ -12,12 +14,18 @@ def get_xml_doc_from_html(html):
     except AttributeError:
         pass
 
+    # Empty or missing
     if not html or html.isspace():
         return None
 
+    # Get XML
     try:
         return lxml.html.fromstring(html)
-    except Exception:
+
+    # Parser error, likely no-text HTML (i.e. whole thing is comments or tags)
+    except lxml.etree.ParserError as e:
+        if str(e) == "Document is empty":
+            return None
         log_sys_exception("LXML HTML parse error in `fromstring`")
 
 
