0c6f84e0426d1ca2906960fbbb2f44ad7a0b1079
Kut Akdogan <5658236+kut@users.noreply.github.com>
2020-02-15 11:56:33 -0500

    Allow extraction of links from XML obj

--- neutronium/utils/xpath.py
+++ neutronium/utils/xpath.py
@@ -4,13 +4,13 @@ import lxml.etree
 from neutron.utils.logging import log_sys_exception
 
 
-def get_xml_doc_from_html(html):
+def get_xml_obj_from_html(html):
     # Empty or missing
     if not html:
         return None
 
     try:
-        html = html.encode('utf-8')
+        html = html.encode("utf-8")
     except AttributeError:
         pass
 
@@ -30,6 +30,20 @@ def get_xml_doc_from_html(html):
 
 
 def get_xpath_from_html(html, xpath_string):
-    xml_doc = get_xml_doc_from_html(html)
-    if xml_doc is not None and xpath_string:
-        return xml_doc.xpath(xpath_string)
+    xml_obj = get_xml_obj_from_html(html)
+    if xml_obj is not None and xpath_string:
+        return xml_obj.xpath(xpath_string)
+
+
+def get_good_links_from_xml_obj(xml_obj, titles=False):
+    link_xpath = "//a[not(@rel='nofollow')]"
+    link_url_xpath = f"{link_xpath}/@href"
+
+    if titles:
+        return ((e.get("href").strip(), e.text.strip() if e.text else e.text)
+                for e in xml_obj.xpath(link_xpath)
+                if e and e.get("href"))
+
+    return (h.strip()
+            for h in xml_obj.xpath(link_url_xpath)
+            if h)
