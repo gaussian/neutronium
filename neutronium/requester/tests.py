dbcd0819b9f3f52e28a3ee9f5070ff0981483eab
Kut Akdogan <5658236+kut@users.noreply.github.com>
2022-12-16 08:56:20 -0500

    Refactor requester

---
+++ neutronium/requester/tests.py
@@ -0,0 +1,43 @@
+from django.test import tag
+
+from neutron.tests.enhanced_test_case_base import EnhancedTestCaseBase
+from .requester import Requester
+
+
+class RequestUtilsTestCase(EnhancedTestCaseBase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    @tag("fast", "infrastructure")
+    def test_can_optimize_url(self):
+        data = [
+            # TODO: no longer works, as NYT thinks we've blocked Javascript/ads
+            # ("https://www.nytimes.com/2014/03/29/business/a-florida-engineer-unlocked-the-mystery-of-gms-ignition-flaw.html?_r=2",
+            #  "https://www.nytimes.com/2014/03/29/business/a-florida-engineer-unlocked-the-mystery-of-gms-ignition-flaw.html",
+            #  None),
+            ("https://money.cnn.com/2007/09/26/news/companies/uaw_gm_deal/index.htm?postversion=2007092604",
+             "https://money.cnn.com/2007/09/26/news/companies/uaw_gm_deal/index.htm",
+             None),
+            ("https://money.cnn.com/2007/09/24/news/companies/gm_uaw_strikedeadline/?postversion=2007092412",
+             "https://money.cnn.com/2007/09/24/news/companies/gm_uaw_strikedeadline/",
+             None),
+            ("https://www.law360.com/commercialcontracts/articles/1241729/contractor-must-face-ex-nfler-s-dream-house-ip-claims",
+             "https://www.law360.com/articles/1241729/contractor-must-face-ex-nfler-s-dream-house-ip-claims",
+             None),
+            # TODO: this results in an SSL error, despite not using SSL...so can't use this anymore
+            # ("http://www.pyxisintel.com",
+            #  "https://www.bain.com/industry-expertise/private-equity/pyxis/",
+            #  None),
+            # Bloomberg should show a paywall or bot-catcher, and we should recognize this
+            # TODO: Bloomberg no longer shows a paywall here, so this test fails
+            # ("https://www.bloomberg.com/news/articles/2020-07-09/wells-fargo-is-readying-thousands-of-job-cuts-to-start-this-year?utm_source=email",
+            #  "https://www.bloomberg.com/news/articles/2020-07-09/wells-fargo-is-readying-thousands-of-job-cuts-to-start-this-year",
+            #  "wall"),
+        ]
+
+        r = Requester(try_optimize_url=True, verbose=True)
+        for url, canonical_url, correct_error_code in data:
+            _, _, new_url, _, _, error_code = r.get(url)
+            self.assertEqual(new_url, canonical_url)
+            self.assertEqual(error_code, correct_error_code, url)
