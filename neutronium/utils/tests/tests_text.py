4ee39ef3a8573ab5129b40f8ccec20631105d7ea
Kut Akdogan <5658236+kut@users.noreply.github.com>
2025-02-26 22:35:27 -0500

    Fixes to settings & test config

---
+++ neutronium/utils/tests/tests_text.py
@@ -0,0 +1,25 @@
+from django.test import tag
+
+from neutron.tests.enhanced_test_case_base import EnhancedTestCaseBase
+from neutron.utils.text import normalize_web_text
+
+
+class TextUtilsTestCase(EnhancedTestCaseBase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    @tag("fast", "infrastructure")
+    def test_can_normalize_text(self):
+        data = [
+            ("Hello(R)", "Hello"),
+            ("..Hello©", "..Hello"),
+            ("\n\nthis is some text,\" said the man…\n\n\r\n", "this is some text,\" said the man..."),
+            ("\"Nothing wrong with Nancy’s cough,\"\n\nI said - leaning in",
+             "\"Nothing wrong with Nancy's cough,\"\n\nI said — leaning in"),
+            ("Everyone had an alibi—except John.", "Everyone had an alibi — except John."),
+            ("Everyone had an alibi-except John.", "Everyone had an alibi-except John.")
+        ]
+
+        for text, desired_output in data:
+            self.assertEqual(normalize_web_text(text), desired_output)
